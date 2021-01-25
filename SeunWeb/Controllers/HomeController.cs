e56f411eb73670fbe697984cc878456305ea5fa7
kehinde <kennyt4real.ak@gmail.com>
2021-01-25 20:34:44 +0100

    Fix error on controller

--- SeunWeb/Controllers/HomeController.cs
+++ SeunWeb/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SeunWebsite.Models;
+using SeunWeb.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
