6ea333e74654572e0cef4e228873eacf6f518e21
kehinde <kennyt4real.ak@gmail.com>
2021-01-25 20:24:09 +0100

    Added view pages

--- SeunWeb/Controllers/HomeController.cs
+++ SeunWeb/Controllers/HomeController.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SeunWeb.Models;
+using SeunWebsite.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SeunWeb.Controllers
+namespace SeunWebsite.Controllers
 {
     public class HomeController : Controller
     {
@@ -22,11 +22,87 @@ namespace SeunWeb.Controllers
         {
             return View();
         }
+        public IActionResult GlobalLocation()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult MediaCenter()
+        {
+            return View();
+        }
+
+        public IActionResult OurCulture()
+        {
+            return View();
+        }
+        public IActionResult OurExpert()
+        {
+            return View();
+        }
+        public IActionResult Careers()
+        {
+            return View();
+        }
+        public IActionResult OurPeople()
+        {
+            return View();
+        }
+        public IActionResult OurValues()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
             return View();
         }
+        public IActionResult OurImpact()
+        {
+            return View();
+        }
+        public IActionResult Alumni()
+        {
+            return View();
+        }
+        public IActionResult InclusionDiversity()
+        {
+            return View();
+        }
+        public IActionResult Society()
+        {
+            return View();
+        }
+        public IActionResult SustainAbility()
+        {
+            return View();
+        }
+        public IActionResult RecognitionAccolate()
+        {
+            return View();
+        }
+        public IActionResult OurHistory()
+        {
+            return View();
+        }
+        public IActionResult StrategicParterns()
+        {
+            return View();
+        }
+        public IActionResult MissionInclude()
+        {
+            return View();
+        }
+        public IActionResult UkGenderPayGapReport()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
