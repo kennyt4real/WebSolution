4c1ce4e7e6b2c1f9a3abd2d6a6925b5f1d0d73aa
kehinde <kennyt4real.ak@gmail.com>
2021-01-25 22:22:01 +0100

    Added required css and javascript files

---
+++ SeunWeb/wwwroot/assets/js/custom.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+	$(".hub-banner .four--columns-tablet-one-column img").attr("src","/etc/designs/oliverwymanv2/custom/980-390.png");
+    $(".hub-banner .eight--columns-tablet-one-column img").attr("src","/etc/designs/oliverwymanv2/custom/1300-780.png");
+
+});
