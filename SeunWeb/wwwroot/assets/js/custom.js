f75a53bc1c4e52e63bdfaa0ecd937848654f4272
kehinde <kennyt4real.ak@gmail.com>
2021-01-26 22:54:07 +0100

    Added javascript files and fonts

--- SeunWeb/wwwroot/assets/js/custom.js
+++ SeunWeb/wwwroot/assets/js/custom.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-	$(".hub-banner .four--columns-tablet-one-column img").attr("src","/etc/designs/oliverwymanv2/custom/980-390.png");
-    $(".hub-banner .eight--columns-tablet-one-column img").attr("src","/etc/designs/oliverwymanv2/custom/1300-780.png");
+	$(".hub-banner .four--columns-tablet-one-column img").attr("src","~/assets/designs/980-390.png");
+    $(".hub-banner .eight--columns-tablet-one-column img").attr("src","~/assets/designs/1300-780.png");
 
 });
